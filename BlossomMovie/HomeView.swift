efaa7e49026bb3df7721a4713c3c196b144e6adf
Locus Wong <wongtzswa@gmail.com>
2026-02-10 22:23:41 -0500

    Use GeometryReader to fix the issue that hero title jump on screen.

--- BlossomMovie/HomeView.swift
+++ BlossomMovie/HomeView.swift
@@ -11,42 +11,42 @@ struct HomeView: View {
     var heroTestTitle = Constants.testTitleURL
     
     var body: some View {
-        ScrollView {
-            LazyVStack{
-                AsyncImage(url: URL(string: heroTestTitle)){ image in
-                    image
-                        .resizable()
-                        .scaledToFit()
-                } placeholder: {
-                    ProgressView()
+        GeometryReader{ geo in
+            ScrollView {
+                LazyVStack{
+                    AsyncImage(url: URL(string: heroTestTitle)){ image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: geo.size.width, height: geo.size.height * 0.85)
+                    
+                    HStack{
+                        Button{
+                            
+                        } label: {
+                            Text(Constants.playString)
+                                .ghostButton()
+                        }
+                        
+                        Button{
+                            
+                        } label: {
+                            Text(Constants.downloadString)
+                                .ghostButton()
+                        }
+                        
+                    }
+                    
+                    HorizontalListView(header: Constants.trendingMovieString)
+                    HorizontalListView(header: Constants.trendingTVString)
+                    HorizontalListView(header: Constants.topRatedMovieString)
+                    HorizontalListView(header: Constants.topRatedTVString)
                 }
             }
-            
-            HStack{
-                Button{
-                    
-                } label: {
-                    Text(Constants.playString)
-                        .ghostButton()
-                }
-                
-                Button{
-                    
-                } label: {
-                    Text(Constants.downloadString)
-                        .ghostButton()
-                }
-                
-            }
-            
-            HorizontalListView(header: Constants.trendingMovieString)
-            HorizontalListView(header: Constants.trendingTVString)
-            HorizontalListView(header: Constants.topRatedMovieString)
-            HorizontalListView(header: Constants.topRatedTVString)
         }
-        
-       
-        
     }
 }
 
